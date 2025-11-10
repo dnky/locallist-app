ed2fee54a096989ae00336999891c6affb49235b
Matt Morgan <matt.morgan@thomsonreuters.com>
2025-11-10 18:47:28 +0000

    Remove map if no coordinates for ad

--- pages/details.js
+++ pages/details.js
@@ -182,19 +182,17 @@ export default function AdDetailPage({ ad, tenant }) {
               {ad.phone && <a href={`tel:${ad.phone}`} className={`${styles.btnAction} ${styles.btnPhone}`}><i className="fa-solid fa-phone"></i> Call</a>}
               {ad.email && <a href={`mailto:${ad.email}`} className={`${styles.btnAction} ${styles.btnEmail}`}><i className="fa-solid fa-envelope"></i> Email</a>}
             </div>
-            <div className={styles.sidebarMap}>
-              {ad.lat && ad.lng ? (
+            {/* --- THIS IS THE CHANGE --- */}
+            {/* Only render the map container if coordinates exist */}
+            {ad.lat && ad.lng && (
+              <div className={styles.sidebarMap}>
                 <DynamicMap 
                   ads={mapAds} 
                   initialZoom={13}
                   scrollWheelZoom={false}
                 />
-              ) : (
-                <div className={styles.mapPlaceholder}>
-                  <p>No location provided</p>
-                </div>
-              )}
-            </div>
+              </div>
+            )}
           </aside>
         </div>
       </main>
